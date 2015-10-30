f2f6087123b1c1d715ad3aaaa44312ab0f34a4d2
christianjaka94 <christiangar_94@hotmail.com>
2015-10-30 12:55:39 +0100

    file test changed

--- src/test/java/translator/service/TranslatorServiceTest.java
+++ src/test/java/translator/service/TranslatorServiceTest.java
@@ -20,8 +20,8 @@ public class TranslatorServiceTest {
     TranslatorService translatorService;
     @Test
     public void translateTest() throws Exception {
-        TranslatedText translatedText = translatorService.translate("en", "es", "This is a test of translation service");
-        assertEquals("Esto es una prueba de servicio de traducción",translatedText.getTranslation());
+        TranslatedText translatedText = translatorService.translate("en", "es", "I didn't change, only learned, and learning is not to change is to grow");
+        assertEquals("No cambié, solo aprendí, y aprender no es cambiar es crecer",translatedText.getTranslation());
     }
 
 }
